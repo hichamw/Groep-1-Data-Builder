80f1dbdfc3989927d94606fa3cc21ff3c4d38fc8
Lars van Hilten <larsvh8@gmail.com>
2015-03-14 14:46:47 +0100

    Created java project
    -  added mysql library
    - added Controller class
    - and some relevant comments

---
+++ src/Controller.java
@@ -0,0 +1,12 @@
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//zelfde als driver class in telefoonboek project
+		//connectToDatabase functie van miner/server kan gebruikt worden om te connecten
+		//we hebben nog wel een probleempje aangezien we maar 1 user hebben om in te loggen (en dus server gebruikt deze EN client = errors)
+
+	}
+
+}
