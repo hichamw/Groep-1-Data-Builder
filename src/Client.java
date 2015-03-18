830afeaabd35b2757b58dc5135a3a7240065974e
stevejongbloed <stevejongbloed@gmail.com>
2015-03-18 15:42:46 +0100

    steve
    - hashtags shit

--- src/Client.java
+++ src/Client.java
@@ -5,52 +5,79 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Client {
-	private static Connection conn = null;
-	private static ArrayList <String> data = new ArrayList <String>();
-	private static ResultSet result;
-	private static PreparedStatement statement;
-	
-	public static void connectToDatabase(){
-		
+	private Connection conn = null;
+	private ArrayList<Hashtag> hashtags = new ArrayList<Hashtag>();
+	private ResultSet result;
+	private PreparedStatement statement;
+
+	public void connectToDatabase() {
 		String host = "sql3.freemysqlhosting.net";
 		String DBName = "sql369437";
 		String user = "sql369437";
 		String password = "yH1%hH1*";
 		String encPassword = URLEncoder.encode(password);
 		try {
-            conn = DriverManager.getConnection("jdbc:mysql://" + host +"/"+ DBName +"?user=" + user + "&password=" + encPassword + "");
-            System.out.println("Database connected!");
-           
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }
-	
-	
-    }
-	
-	public static void extractDataFromDatabase() throws SQLException {
-		statement = conn.prepareStatement("SELECT * FROM Twitter");		
+			conn = DriverManager.getConnection("jdbc:mysql://" + host + "/"
+					+ DBName + "?user=" + user + "&password=" + encPassword
+					+ "");
+			System.out.println("Database connected!");
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}  
+
+	}
+
+	public void extractDataFromDatabase() throws SQLException {
+		statement = conn.prepareStatement("SELECT Content FROM Twitter");
 		result = statement.executeQuery();
 
-		while(result.next()){
-			// result.getString() extract een column naar keuze uit onze Twitter database.
-			// column 1 = Username column 2 = Name etc.		
-			System.out.println(result.getString(6));
+		try {
+			while (result.next()) {
+				// result.getString() extract een column naar keuze uit onze
+				// Twitter database.
+				// column 1 = Username column 2 = Name etc.
+				//String[] info = new String[1];
+				ArrayList<String> info = new ArrayList<String>();
+				String[] test = result.getString(1).split("[\\s]");
+				info = (ArrayList<String>) Arrays.asList(test);
+				for (int i = 0; i <= info.size(); i++) {
+					
+					if (info[i].charAt(0) == '#') {
+						
+						String temp = info[i];
+						for (Hashtag h : hashtags) {
+							if (h.getName().equals(temp)) {
+								h.increment();
 
+							} else {
+								hashtags.add(h);
+							}
+
+						}
+
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		
+		for (Hashtag h : hashtags) {
 
-		
+			System.out.println(h.getName());
+			System.out.println(h.getCount());
+
+		}
+	}
+
 }
-}
-
-
-
