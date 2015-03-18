43fb3540c0ab6cdb493be71b0d308be7b84ecb75
gamerszvidx <gamerszvidx@hotmail.com>
2015-03-18 14:37:03 +0100

    controller makes connecton

--- src/Data.java
+++ src/Data.java
@@ -8,34 +8,12 @@ import java.util.ArrayList;
 
 
 public class Data {
-	private static Connection conn = null;
 	private static ArrayList <String> data = new ArrayList <String>();
 	private static ResultSet result;
 	private static PreparedStatement statement;
 	
-	public static void connectToDatabase(){
-		
-		String host = "sql3.freemysqlhosting.net";
-		String DBName = "sql369437";
-		String user = "sql369437";
-		String password = "yH1%hH1*";
-		String encPassword = URLEncoder.encode(password);
-		try {
-            conn = DriverManager.getConnection("jdbc:mysql://" + host +"/"+ DBName +"?user=" + user + "&password=" + encPassword + "");
-            System.out.println("Database connected!");
-           
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }
-	
-	
-    }
-	
 	public static void extractDataFromDatabase() throws SQLException {
-		statement = conn.prepareStatement("SELECT Language FROM Twitter");		
+		statement = Controller.conn.prepareStatement("SELECT Language FROM Twitter");		
 		result = statement.executeQuery();
 		int nl = 0;
 		int en = 0;
